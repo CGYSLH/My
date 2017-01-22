9fdcf8c3117f2c3d75709ccf7354e4b2a9010938
zpw_mariya <hnzhangpengwei@163.com>
2017-01-22 11:49:46 +0800

    获取提示的文本的选择的内容的方法

--- autocompletextview带自动提示的文本输入框/src/main/java/com/mi/autocompletextview/MainActivity.java
+++ autocompletextview带自动提示的文本输入框/src/main/java/com/mi/autocompletextview/MainActivity.java
@@ -2,8 +2,11 @@ package com.mi.autocompletextview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,6 +24,13 @@ public class MainActivity extends AppCompatActivity {
         actv = (AutoCompleteTextView) findViewById(R.id.actv);
         initData();
         initAdapter();
+        actv.setOnItemClickListener(new AdapterView.OnItemClickListener() {//监听选择的是那个
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Toast.makeText(MainActivity.this,position+"",Toast.LENGTH_SHORT).show();//这只能够获取位置无法获取内容
+                Toast.makeText(MainActivity.this,parent.getItemAtPosition(position).toString(),Toast.LENGTH_SHORT).show();//如果哟获取内容可以采用这样的方法
+            }
+        });
     }
 
     private void initAdapter() {
